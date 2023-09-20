9c1ac78e37b4f009e86abb9e91bde2c1eaf00304
Shkliarova <130659546+Shkliarova@users.noreply.github.com>
2023-09-20 19:22:31 +0000

    Deploying to gh-pages from @ Shkliarova/goit-js-hw-09@ad2e573524a39c06e1dc98aeddbb81adcd8726f7 🚀

---
+++ 01-color-switcher.0b649acb.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=document.querySelector("body");t.addEventListener("click",(function(){n||(a(),n=setInterval(a,1e3),t.disabled=!0,e.disabled=!1)})),e.addEventListener("click",(function(){n&&(clearInterval(n),n=null,t.disabled=!1,e.disabled=!0)}));let n=null;function a(){d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}
+//# sourceMappingURL=01-color-switcher.0b649acb.js.map
